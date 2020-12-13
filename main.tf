d21da11cacb8600d17bac673a46e7f2ff1ed44a6
anuj-aws1234 <62469532+anuj-aws1234@users.noreply.github.com>
2020-12-13 10:59:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform{
+  backend  "s3"{
+    bucket = "terra-bucket-first"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    }
+  }
+
+
 provider "aws" {
   region     = "us-east-1"
   #access_key = "################"
